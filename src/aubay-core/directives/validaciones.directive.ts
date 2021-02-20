111f70bb73c758eafe46cbc5dd780d70c0d9c5e8
Javier <jmartin@grupoloyal.com>
2021-02-20 10:22:59 +0100

    validaciones

--- src/aubay-core/directives/validaciones.directive.ts
+++ src/aubay-core/directives/validaciones.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, forwardRef, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Directive, forwardRef, OnChanges, Input, SimpleChanges, ElementRef } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS, ValidatorFn, ValidationErrors, Validators } from '@angular/forms';
 
 export const isUpperCase = (valor: string) => valor !== valor.toUpperCase();
@@ -18,23 +18,51 @@ export class UpperCaseValidator implements Validator {
   }
 }
 
-export const isEsNIF = (valor: string) => true;
+export function NIFValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) { return null; }
+      const err = { nif: { invalidFormat: true, invalidChar: true } };
+      if (/^\d{1,8}\w$/.test(control.value)) {
+          const letterValue = control.value.substr(control.value.length - 1);
+          const numberValue = control.value.substr(0, control.value.length - 1);
+          err.nif.invalidFormat = false;
+          return letterValue.toUpperCase() === 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(numberValue % 23)
+              ? null : err;
+      } else { return err; }
+  };
+}
 
 @Directive({
-  selector: '[esnif]',
-  providers: [{ provide: NG_VALIDATORS, useExisting: EsNIFValidator, multi: true }]
+  // tslint:disable-next-line:directive-selector
+  selector: '[nif][formControlName],[nif][formControl],[nif][ngModel]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: NIFValidatorDirective, multi: true }]
 })
-export class EsNIFValidator implements Validator {
-  validate(control: AbstractControl): { [key: string]: any } | null {
-      const valor = control.value;
-      if (valor) {
-          return isEsNIF(valor) ? { 'esnif': `${valor} no es un NIF` } : null;
-      } else {
-          return null;
-      }
+export class NIFValidatorDirective implements Validator {
+  validate(control: AbstractControl): { [key: string]: any } {
+      return NIFValidator()(control);
   }
 }
 
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[type][formControlName],[type][formControl],[type][ngModel]',
+  providers: [
+      { provide: NG_VALIDATORS, useExisting: forwardRef(() => TypeValidatorDirective), multi: true }
+  ]
+})
+export class TypeValidatorDirective implements Validator {
+  constructor(private elem: ElementRef) { }
+  validate(control: AbstractControl): { [key: string]: any } {
+      const valor = control.value;
+      if (valor) {
+        const dom = this.elem.nativeElement;
+        if (dom.validity) { // dom.checkValidity();
+          return dom.validity.typeMismatch ? { 'type': dom.validationMessage } : null;
+        }
+      }
+      return null;
+  }
+}
 export const MIN_VALIDATOR: any = {
   provide: NG_VALIDATORS,
   useExisting: forwardRef(() => MinValidator),
@@ -97,4 +125,4 @@ export class MaxValidator implements Validator,
   }
 }
 
-export const MIS_VALIDADORES = [ UpperCaseValidator, MinValidator, MaxValidator, EsNIFValidator ];
+export const MIS_VALIDADORES = [ UpperCaseValidator, MinValidator, MaxValidator, NIFValidatorDirective, TypeValidatorDirective ];
